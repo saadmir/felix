ef96fe3cf3388fa964eefbc238bab6a6099f0c71
Saad Mir <saadmir@gmail.com>
2018-01-27 14:43:40 -0800

    with ohana api call

--- src/search.controller.ts
+++ src/search.controller.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import * as _ from 'lodash';
+import fetch from 'node-fetch';
 import * as Types from './types';
 
 export const search = (req: Request, res: Response, next: NextFunction) => {
   console.log('[search.controller]', 'query', req.params);
 
   return Promise.resolve().then(
+    () => {
+      return fetch('http://54.177.38.37:8080/api/search?lat_lng=37.468614,-122.2067982&category=Health');
+    }
+  ).then(
+    res => res.json()
+  ).then(
     (data) => {
-      res.json({success: true, data: req.params}).end();
+      res.json({success: true, data}).end();
     }
   ).catch(
     (err) => {
