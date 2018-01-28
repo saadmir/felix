00d7a4b1cd9e27a5bf3c519f385f37134e594f4a
Saad Mir <saadmir@gmail.com>
2018-01-27 22:30:30 -0800

    with firebase update

--- src/search.controller.ts
+++ src/search.controller.ts
@@ -3,15 +3,26 @@ import * as _ from 'lodash';
 import fetch from 'node-fetch';
 import * as Types from './types';
 import * as uuidv1 from 'uuid/v1';
+import * as firebase from 'firebase-admin';
 
 import { DialogFlow } from './utils/DialogFlow';
 import { HashObject } from './types';
 
+
+const serviceAccount = require("../firebase.json");
+
 const sessions: HashObject = {};
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://hacks-8372f.firebaseio.com"
+});
 
 export const search = (req: Request, res: Response, next: NextFunction) => {
   console.log('[search.controller]', 'query', req.query);
 
+  const db = firebase.database();
+
+  let dialogFlowResponse = {};
   const response: HashObject = {
     sessionId: req.query.sessionId || uuidv1(),
     data: [],
@@ -28,6 +39,7 @@ export const search = (req: Request, res: Response, next: NextFunction) => {
   ).then(
     (data: HashObject) => {
       console.log('[search.controller]', 'data', data);
+      dialogFlowResponse = data;
       const result = (data || {}).result || {};
       console.log('[search.controller]', 'action', result.action || '');
       response.text = (result.fulfillment || {}).speech || '';
@@ -52,11 +64,28 @@ export const search = (req: Request, res: Response, next: NextFunction) => {
   ).then(
     (data) => {
       if (data) {
-        response.data = data;
+        if (data.length) {
+          response.data = data;
+        } else {
+          response.text = process.env.EMPTY_RESULT_TEXT;
+        }
       }
     }
   ).then(
     () => {
+      if (req.query.phone) {
+        const userRef = db.ref(req.query.phone);
+        const ref1 = userRef.child(response.sessionId);
+        const ref2 = ref1.push();
+        ref2.set({
+          timestamp: Date.now(),
+          sessionId: response.sessionId,
+          request: req.query,
+          response: response,
+          dialogFlowResponse: JSON.stringify(dialogFlowResponse)
+        }).catch((err) => console.log('firebase error'));
+      }
+
       res.json(response).end();
     }
   ).catch(
