a4868806537a9d7f10edadc62a60bc7a79b7cbd7
Saad Mir <saadmir@gmail.com>
2018-01-27 18:16:14 -0800

    with dialogflow interaction

--- src/search.controller.ts
+++ src/search.controller.ts
@@ -3,12 +3,25 @@ import * as _ from 'lodash';
 import fetch from 'node-fetch';
 import * as Types from './types';
 
+
+import { DialogFlow } from './utils/DialogFlow';
+import { HashObject } from './types';
+
 export const search = (req: Request, res: Response, next: NextFunction) => {
   console.log('[search.controller]', 'query', req.query);
 
   return Promise.resolve().then(
     () => {
-      return fetch(`https://api.smc-connect.org/search?lat_lng=${req.query.lat},${req.query.lng}&category=Health`);
+      return DialogFlow.Search(req.query.queryText || 'health');
+    }
+  ).then(
+    (data: HashObject) => {
+      console.log('[search.controller]', 'result', ((data || {}).result || {}).action || '');
+      return (((data || {}).result || {}).action || 'health').replace(/FX\-/i, '');
+    }
+  ).then(
+    (category) => {
+      return fetch(`https://api.smc-connect.org/search?lat_lng=${req.query.lat},${req.query.lng}&category=${category}`);
     }
   ).then(
     res => res.json()
